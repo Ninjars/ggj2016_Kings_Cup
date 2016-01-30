9af61db5ad4b5886747f7308fbee45491190af65
Jeremy Stevens <jez@Viper.london.local>
2016-01-30 20:21:07 +0000

    GameManager now explains why the game ended

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -72,10 +72,23 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	//GameOver is called when the player reaches 0 food points
-		//Set levelText to display number of levels passed and game over message
-		//Enable black background image gameObject.
-		ShowLevelImage("You were removed from court");
 	public void GameOver(GameOverReason reason) {
+		string failMessage = "YOU WERE REMOVED FROM COURT\n";
+		switch (reason) {
+			case GameOverReason.WINE:
+			failMessage += "You ran out of wine before reaching the King.";
+			break;
+			case GameOverReason.TIME:
+			failMessage += "You ran out of time before reaching the King.";
+			break;
+			case GameOverReason.SHADOWS:
+			failMessage += "You spent too long skulking in the shadows.";
+			break;
+			case GameOverReason.ATTEMPTED_REGICIDE:
+			failMessage += "Bodily assaulting the King is generally frowned upon.";
+			break;
+		}
+		ShowLevelImage(failMessage);
 
 		//Disable this GameManager.
 		enabled = false;
